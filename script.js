c120be7b626f83d6946e6bf438d719d1a5239ee8
Cyberse-Glitch <nathaniel.darcy@gmail.com>
2025-05-15 11:59:05 +0100

    Java files added
    Added Javascript.
    
    Worked and practiced with coding.

---
+++ script.js
@@ -0,0 +1,71 @@
+ // console.log ("hello freindzo");
+
+ // const multLineSentence = "this a /nsentence/another line here";
+
+ //const insertedVariable = "INserted VARIABLE";
+ // console.log (" This is an " + insertedVariable);
+
+ //const variableSentence = `This is an $ {inserted variable}`;
+
+ // console.log (" who are you people");
+
+ // console.log(variableSentence. length);
+
+ // let userInput = "hElLo LiArM";
+
+
+ // let lowerCase = userInput.toLocaleLowerCase();
+
+
+
+ // console.log (userInput);
+
+
+
+ // let fullSentence = "ths is a sentende for doing things like this string manipulation. pSascha is here to. sascha";
+
+ // fullSentence = fullSentence.toLowerCase();
+
+ // console.log (fullSentence[fullSentence.length - 1]);
+
+ // let lengthSentence = fullSentence.length;
+
+ // let variablCase = fullSentence [lengthSentence];
+
+ // console.log(lengthSentence);
+
+
+ // let includeSascha = fullSentence.includes ("sascha");
+ // console.log(includeSascha); 
+
+ // let findSascha = fullSentence.indexOf ("sascha");
+
+ // console.log(findSascha); 
+
+ // let getSaschaOut = fullSentence.slice(67,72);
+
+ // console.log (getSaschaOut);
+
+ // let replaceSascha = fullSentence.replace ("sascha", "mike");
+
+ // console.log (replaceSascha); 
+
+ // let userPrompt = prompt (" choose a number");
+
+// console.log ("userPrompt = "  + userPrompt);
+ // console.log ("userPrompt type =" + typeof userPrompt);
+
+ //  console.log(parseFloat (userPrompt)); 
+
+ const remainder = 10 % 5;
+
+ console.log(remainder);
+
+ let useMath = Math.ceil(7.2); // 4
+ console.log(useMath);
+
+ let randomNum = Math.random() * 20 +1;
+
+ 
+
+ console.log (randomNum);
